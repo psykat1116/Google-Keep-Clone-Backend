a1a3e021d180198821d174d3158e54a3f38429b9
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2023-09-11 14:11:33 +0530

    Connect To Atlas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("Connected To Database Successfully")
 }).catch((err) => {
     console.log(err)
